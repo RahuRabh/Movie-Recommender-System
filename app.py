74287203018f4897daf7a079c3fd0af34440e25f
Rahul Kumar <63224718+RahuRabh@users.noreply.github.com>
2022-10-29 11:09:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+#importing the required libraries
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
 
+#function to fetch poster from tmdb api
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=63bf0eca73e15dd7af11ea0fbf30e842&language=en-US'.format(movie_id))
     data = response.json()
@@ -10,6 +12,7 @@ def fetch_poster(movie_id):
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
+#function to recommend top 5 movies 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -19,26 +22,27 @@ def recommend(movie):
     recommended_movies_poster = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        #fetch poster
         recommended_movies.append(movies.iloc[i[0]].title)
+        #fetching poster
         recommended_movies_poster.append(fetch_poster(movie_id))
     return recommended_movies, recommended_movies_poster
 
-
+#reading the movie list using the pickel file
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
+#title
 st.title('Movie Recommender System')
 
+#reading the similarity pickel file
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-
-# selected_movie_name = st.selectbox('Type or select a movie from the dropdown', movies_list)
-
+#list of movie name in the select box
 selected_movie_name = st.selectbox(
     'Let me recommend a movie:',
     movies['title'].values)
 
+#recommend button which shows the 5 recommended movies 
 if st.button('Recommend'):
     names, posters = recommend(selected_movie_name)
     col1, col2, col3, col4, col5 = st.columns(5)
